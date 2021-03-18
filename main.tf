29f0b543bf2ab0633176fc3521cf082959db1092
Louis Lamia <louis.lamia@ellevationeducation.com>
2021-03-18 12:08:45 -0400

    Try building deployment package here

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ data "aws_iam_policy" "cloudwatch_readonly" {
   arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
 
+data "archive_file" "deployment_package" {
+  type        = "zip"
+  source_dir  = "${path.module}/vendor/aws-to-slack/build"
+  output_path = "${path.module}/aws-to-slack.zip"
+}
+
 module "lambda" {
   source = "git::https://github.com/ellevation/terraform-aws-lambda.git"
 
@@ -15,7 +21,7 @@ module "lambda" {
   runtime       = "nodejs10.x"
   timeout       = 10
 
-  deployment_package_path = "${path.module}/vendor/aws-to-slack/aws-to-slack.zip"
+  deployment_package_path = data.archive_file.deployment_package.output_path
 
   policy = {
     json = data.aws_iam_policy.cloudwatch_readonly.policy
